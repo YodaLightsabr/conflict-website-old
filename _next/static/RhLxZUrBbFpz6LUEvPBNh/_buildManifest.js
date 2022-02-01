d1795be16802de94a4aa6c34f9019e926ca86219
YodaLightsabr <76178582+YodaLightsabr@users.noreply.github.com>
2022-02-01 01:47:39 +0000

    Update gh-pages to output generated at 52ea56e

---
+++ _next/static/RhLxZUrBbFpz6LUEvPBNh/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/5f5916d60b3762c6.css","static/chunks/pages/index-e9e7ad02a4d45457.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
